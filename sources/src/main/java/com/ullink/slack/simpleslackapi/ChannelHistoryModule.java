ca164328ed5921f633982337343914b39f9b2774
Craig O'Shannessy <craig@perfectlogic.com.au>
2017-02-12 03:38:39 +1100

    Fixed channelName -> channelId discrepancy between ChannelHistoryModule and ChannelHistoryModuleImpl

--- sources/src/main/java/com/ullink/slack/simpleslackapi/ChannelHistoryModule.java
+++ sources/src/main/java/com/ullink/slack/simpleslackapi/ChannelHistoryModule.java
@@ -6,13 +6,13 @@ import com.ullink.slack.simpleslackapi.events.SlackMessagePosted;
 
 public interface ChannelHistoryModule {
     
-    List<SlackMessagePosted> fetchHistoryOfChannel(String channelName);
+    List<SlackMessagePosted> fetchHistoryOfChannel(String channelId);
 
-    List<SlackMessagePosted> fetchHistoryOfChannel(String channelName, LocalDate day);
+    List<SlackMessagePosted> fetchHistoryOfChannel(String channelId, LocalDate day);
 
-    List<SlackMessagePosted> fetchHistoryOfChannel(String channelName, int numberOfMessages);
+    List<SlackMessagePosted> fetchHistoryOfChannel(String channelId, int numberOfMessages);
 
-    List<SlackMessagePosted> fetchHistoryOfChannel(String channelName, LocalDate day, int numberOfMessages);
+    List<SlackMessagePosted> fetchHistoryOfChannel(String channelId, LocalDate day, int numberOfMessages);
 
     SlackMessagePosted fetchSingleMessage(String channelId, String messageId);
 
